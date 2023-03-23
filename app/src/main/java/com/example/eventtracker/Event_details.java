eb7d1b9a7f082508f16f62ab0b969ec061c67280
badal1302 <badalpatel539@gmail.com>
2023-03-23 01:07:27 -0500

    completed Event Details page...

--- app/src/main/java/com/example/eventtracker/Event_details.java
+++ app/src/main/java/com/example/eventtracker/Event_details.java
@@ -1,10 +1,9 @@
 package com.example.eventtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Event_details extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
